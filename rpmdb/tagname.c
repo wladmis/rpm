e4d02edfd1aadf032d436a0c58ba0fa27c5a78b8
Dmitry V. Levin <ldv@altlinux.org>
2005-10-15 14:57:00 +0000

    relocated tagName.c from lib to rpmdb

--- rpmdb/tagname.c
+++ rpmdb/tagname.c
@@ -1,5 +1,5 @@
 /**
- * \file lib/tagName.c
+ * \file rpmdb/tagname.c
  */
 
 #include "system.h"
@@ -16,35 +16,33 @@ const char *const tagName(int tag)
     switch (tag) {
     case RPMDBI_PACKAGES:
 	strcpy(nameBuf, "Packages");
-	return nameBuf;
-	/*@notreached@*/ break;
+	break;
     case RPMDBI_DEPENDS:
 	strcpy(nameBuf, "Depends");
-	return nameBuf;
-	/*@notreached@*/ break;
+	break;
     case RPMDBI_ADDED:
 	strcpy(nameBuf, "Added");
-	return nameBuf;
-	/*@notreached@*/ break;
+	break;
     case RPMDBI_REMOVED:
 	strcpy(nameBuf, "Removed");
-	return nameBuf;
-	/*@notreached@*/ break;
+	break;
     case RPMDBI_AVAILABLE:
 	strcpy(nameBuf, "Available");
-	return nameBuf;
-	/*@notreached@*/ break;
-    }
-
-    strcpy(nameBuf, "(unknown)");
-    for (i = 0; i < rpmTagTableSize; i++) {
-	if (tag != rpmTagTable[i].val)
-	    continue;
-	nameBuf[0] = nameBuf[1] = '\0';
-	if (rpmTagTable[i].name != NULL)	/* XXX programmer error. */
-	    strcpy(nameBuf, rpmTagTable[i].name + 7);
-	for (s = nameBuf+1; *s != '\0'; s++)
-	    *s = xtolower(*s);
+	break;
+    default:
+	strcpy(nameBuf, "(unknown)");
+/*@-boundswrite@*/
+	for (i = 0; i < rpmTagTableSize; i++) {
+	    if (tag != rpmTagTable[i].val)
+		continue;
+	    nameBuf[0] = nameBuf[1] = '\0';
+	    if (rpmTagTable[i].name != NULL)	/* XXX programmer error. */
+		strcpy(nameBuf, rpmTagTable[i].name + (sizeof("RPMTAG_")-1));
+	    for (s = nameBuf+1; *s != '\0'; s++)
+		*s = xtolower(*s);
+	    /*@loopbreak@*/ break;
+	}
+/*@=boundswrite@*/
 	break;
     }
     return nameBuf;
@@ -54,19 +52,19 @@ int tagValue(const char * tagstr)
 {
     const struct headerTagTableEntry_s *t;
 
-    if (!strcmp(tagstr, "Packages"))
+    if (!xstrcasecmp(tagstr, "Packages"))
 	return RPMDBI_PACKAGES;
-    if (!strcmp(tagstr, "Depends"))
+    if (!xstrcasecmp(tagstr, "Depends"))
 	return RPMDBI_DEPENDS;
-    if (!strcmp(tagstr, "Added"))
+    if (!xstrcasecmp(tagstr, "Added"))
 	return RPMDBI_ADDED;
-    if (!strcmp(tagstr, "Removed"))
+    if (!xstrcasecmp(tagstr, "Removed"))
 	return RPMDBI_REMOVED;
-    if (!strcmp(tagstr, "Available"))
+    if (!xstrcasecmp(tagstr, "Available"))
 	return RPMDBI_AVAILABLE;
 
     for (t = rpmTagTable; t->name != NULL; t++) {
-	if (!xstrcasecmp(t->name + 7, tagstr))
+	if (!xstrcasecmp(t->name + (sizeof("RPMTAG_")-1), tagstr))
 	    return t->val;
     }
     return -1;
