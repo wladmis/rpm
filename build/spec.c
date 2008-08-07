33a4004746b0f10bdeafe3cea82b77b5aba37ca7
Jindrich Novy <jnovy@redhat.com>
2008-08-07 14:50:49 +0200

    Don't allow multiple definition of PatchX or SourceX (rhbz#458261, rhbz#458260)

--- build/spec.c
+++ build/spec.c
@@ -308,6 +308,16 @@ int addSource(Spec spec, Package pkg, const char *field, int tag)
 	}
     }
 
+    /* Check whether tags of the same number haven't already been defined */
+    for (p = spec->sources; p != NULL; p = p->next) {
+	if ( p->num != num ) continue;
+	if (tag == RPMTAG_SOURCE && p->flags == RPMBUILD_ISSOURCE ||
+	    tag == RPMTAG_PATCH  && p->flags == RPMBUILD_ISPATCH) {
+		rpmlog(RPMLOG_ERR, _("%s %d defined multiple times\n"), name, num);
+		return RPMRC_FAIL;
+	    }
+    }
+
     /* Create the entry and link it in */
     p = xmalloc(sizeof(*p));
     p->num = num;
