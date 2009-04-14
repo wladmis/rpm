71ba99f754ea8ba8379151bc173ea4959936e44d
Dmitry V. Levin <ldv@altlinux.org>
2009-04-14 16:43:52 +0000

    tools/rpm*cmp.c: Reindent

--- tools/rpmevrcmp.c
+++ tools/rpmevrcmp.c
@@ -25,33 +25,33 @@
 #include <rpmlib.h>
 
 int
-main (int ac, const char *av[])
+main(int ac, const char *av[])
 {
 	const char *e1 = 0, *v1 = 0, *r1 = 0;
 	const char *e2 = 0, *v2 = 0, *r2 = 0;
-	char *arg1, *arg2;
+	char   *arg1, *arg2;
 
 	if (ac != 3)
 	{
-		fprintf (stderr,
-			 "%s - compare EVRs.\n"
-			 "Usage: %s <EVR1> <EVR2>\n"
-			 "\nReport bugs to http://bugs.altlinux.ru/\n\n",
-			 program_invocation_short_name,
-			 program_invocation_short_name);
+		fprintf(stderr,
+			"%s - compare EVRs.\n"
+			"Usage: %s <EVR1> <EVR2>\n"
+			"\nReport bugs to http://bugs.altlinux.ru/\n\n",
+			program_invocation_short_name,
+			program_invocation_short_name);
 		return EXIT_FAILURE;
 	}
 
-	arg1 = strdup (av[1]);
-	parseEVR (arg1, &e1, &v1, &r1);
+	arg1 = strdup(av[1]);
+	parseEVR(arg1, &e1, &v1, &r1);
 
-	arg2 = strdup (av[2]);
-	parseEVR (arg2, &e2, &v2, &r2);
+	arg2 = strdup(av[2]);
+	parseEVR(arg2, &e2, &v2, &r2);
 
-	printf ("%d\n", rpmEVRcmp (e1, v1, r1, 0, e2, v2, r2, 0));
+	printf("%d\n", rpmEVRcmp(e1, v1, r1, 0, e2, v2, r2, 0));
 
-	free (arg2);
-	free (arg1);
+	free(arg2);
+	free(arg1);
 
 	return EXIT_SUCCESS;
 }
