afe967774229612d7069a58e11b46888a1151d40
Alexey Tourbin <at@altlinux.ru>
2011-01-04 09:05:29 +0300

    tools/setcmp.c: with argc==1, read versions from stdin

--- tools/setcmp.c
+++ tools/setcmp.c
@@ -1,11 +1,16 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include "set.h"
 
-int main(int argc, const char **argv)
+static
+int setcmp(const char *s1, const char *s2)
 {
-    assert(argc == 3);
-    int cmp = rpmsetcmp(argv[1], argv[2]);
+    int cmp = rpmsetcmp(s1, s2);
     switch (cmp) {
     case 1:
     case 0:
@@ -24,4 +29,24 @@ int main(int argc, const char **argv)
 	break;
     }
     return 1;
+}
+
+int main(int argc, const char **argv)
+{
+    assert(argc == 1 || argc == 3);
+    if (argc == 3)
+	return setcmp(argv[1], argv[2]);
+    int rc = 0;
+    while (1) {
+	char *s1 = NULL, *s2 = NULL;
+	int n = scanf("%as %as", &s1, &s2);
+	if (n == EOF)
+	    break;
+	assert(n == 2);
+	assert(s1 && s2);
+	rc |= setcmp(s1, s2);
+	free(s1);
+	free(s2);
+    }
+    return rc;
 }
